5c765fff3f497d17a799b7a50c047794cf366ebe
PriyankaNigade18 <piyu1818@gmail.com>
2026-08-20 10:25:17 +0530

    Javascript Day-4

--- JsBasics/P003_Variables.js
+++ JsBasics/P003_Variables.js
@@ -104,3 +104,133 @@ console.log("FirstName: "+fname+"\n & location: "+location+"\n & emailId: "+emai
 //call
 
 show();
+
+
+console.log("-----function scope for var------");
+/*
+Any variable declare inside function we can call it within function that is function
+scope/ local scope
+var is function scope but let and const are block scope
+*/
+
+
+function info()
+{
+    var automationType='Functional';
+    let browserVersion=150;
+    const browserName="chrome";
+
+    console.log("vatype:"+automationType);
+    console.log("let type: "+browserVersion);
+    console.log("const type: "+browserName);
+    
+      
+}
+
+//call
+info();
+
+
+console.log("----outside the function----");
+
+    //console.log("var type:"+automationType);//ReferenceError: automationType is not defined
+    //console.log("let type: "+browserVersion);//ReferenceError: browserVersion is not defined
+    //console.log("const type: "+browserName);//ReferenceError: browserName is not definedlog
+
+    console.log("---------");
+    
+var a=10;
+console.log(a);//10
+
+function test1()
+{
+    var a=20;
+    console.log(a);//20
+    if(true)
+    {
+        var a=30;
+        console.log(a);//30
+    }
+    console.log(a);//30
+    
+}
+
+test1();
+
+console.log("----block scope-----");
+/*
+Any variable declared inside block { }, if block ,
+for loop,while loop we can access it within block
+let and const are block scoped
+
+
+*/
+
+if(true)
+{
+    let testType="API";
+    const tool="Postman";
+    console.log(testType);
+    console.log(tool);
+       
+}
+
+    //console.log(testType);//ReferenceError: testType is not defined
+    //console.log(tool);//ReferenceError: tool is not defined
+
+
+let b=10;//Global scope
+console.log(b);//10
+
+function test2()
+{
+    let b=20;//function scope
+    console.log(b);//20
+    if(true)
+    {
+        let b=30;//block scope
+        console.log(b);//30
+        
+    }
+    console.log(b);//20
+    }
+
+test2();
+
+//global
+console.log(b);//10
+
+console.log(a);//10
+
+//const=Global+block scope
+
+
+const c=10;
+console.log(c);//10
+
+function test3()
+{
+    const c=20;
+    console.log(c); //20
+
+    if(true)
+    {
+        const c=30;
+        console.log(c);//30
+        
+    }
+    console.log(c);//20
+    
+    
+}
+
+test3();
+
+
+
+
+
+
+
+
+
