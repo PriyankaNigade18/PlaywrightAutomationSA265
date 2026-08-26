507bc9ba3ac66d4c60edc4cdf3f30b2e0c4e534f
PriyankaNigade18 <piyu1818@gmail.com>
2026-08-26 10:27:36 +0530

    Javascript Day-6

--- JsBasics/P006_Operator.js
+++ JsBasics/P006_Operator.js
@@ -76,9 +76,185 @@ value will use first and then increment
 
 
 --(Decrement)
+--------------------------
+1.Predecrement --num1    num1=num1-1
+----------------------------------------
+value will decrement first and then use it
+
+2.Postdecrement  num1-- num1=num-1
+---------------------------------------
+value will use first and then decrement
+
 
 */
 
+//increment
+
+//pre
+let a=100;
+console.log(a);//100
+console.log(++a);//101
+console.log(a);//101
+
+
+//post
+let b=200;
+console.log(b);//200
+console.log(b++);//200
+console.log(b);//201
+console.log(b);//201
+console.log(b++);//201
+console.log(b);//202
+
+let i=199;
+let j=i++;
+console.log(i);//200
+console.log(j);//199
+
+console.log(j);//199
+console.log(i);//200
+
+let p=678;
+let q=p++;
+console.log(p);//679
+console.log(q);//678
+
+
+let r=78;
+let s=++r;
+console.log(r);//79
+console.log(s);//79
+
+let u=101;
+let v=++u;
+console.log(u);//102
+console.log(v);//102
+
+//post decrement
+
+let e=90;
+let f=e--;
+console.log(e);//89
+console.log(f);//90
+
+let t=100;
+t=t--;
+console.log(t);//99
+
+let y=89;
+let z=--y;
+console.log(y);//88
+console.log(z);//88
+
+
+let m=10;
+let n=--m + m--;
+console.log(m);//8
+console.log(n);//18
+
+console.log("-----short hand operators-----");
+/*
++= -= *= /= %= (eg. x=x+1  insted x+=1)
+*/
+
+let k=10;
+console.log(k);//10
+k+=20;//k=k+20
+console.log(k);//30
+
+
+let l=100;
+l-=50;//l=l-50
+console.log(l);//50
+
+let o=10;
+o*=10;//o=o*10
+console.log(o);//100
+
+let w=80;
+w/=4;//w=w/4
+console.log(w);//20
+
+console.log("------Relational operators-------");
+/*
+>,>=,<,<=,!=
+equality
+----------------
+1.loose equality ==
+2.strict equality === recommended
+
+testData
+-----------
+y=z=88 m=8 n=18 u=v=102 e=89 f=90 t=99
+*/
+
+console.log("greater than > : "+(u>z));//true
+console.log("greater than equal to >=: "+(t>=y));//true
+console.log(u>=v);//true
+console.log(m>=f);//false
+console.log("less than < : "+(m<n));//true
+console.log("less than equal to <=: "+(y<=z));//true
+console.log(t<=e);//false
+console.log("not equal to !=: "+(y!=z));//false
+console.log(y!=u);//true
+
+/*equality
+--------------
+1.loose equality ==
+-------------------
+In loose equality value will coerced(type will convert) first and then compared
+
+
+2.strict equality === recommended
+---------------------------------
+Value will compared as it is and type coercion is not applicable
+
+
+*/
+//loose equality
+console.log("100" == 100);//true
+console.log(null == undefined);//true
+
+//strict equality 
+console.log("100" === 100);//false
+console.log(null === undefined);//false
+
+console.log("-------Logical Operators----------");
+/*
+ c1     c2         &&(AND)        ||(OR)       !c1(NOT)
+ true   true        true            true        false
+ true   false       false           true        false
+ false  true        false           true         true
+ false false        false           false        true
+
+ testData
+-----------
+y=z=88 m=8 n=18 u=v=102 e=89 f=90 t=99
+
+*/
+
+console.log("-----&&-----");
+console.log((y===z) && (n>m));//true
+console.log((n>m) && (u!=v));//false
+console.log((t<=f) && (u>y));//false
+console.log((e>=v) && (u<=z));//false
+
+
+console.log("-----||-----");
+console.log((y===z) || (n>m));//true
+console.log((n>m) || (u!=v));//true
+console.log((t<=f) || (u>y));//true
+console.log((e>=v) || (u<=z));//false
+
+
+console.log("----NOT!------");
+console.log(u===v);//true
+console.log(!(u===v));//false
+
+let actTitle="google";
+let expTitle="googleApp";
+console.log("equality:" +(actTitle===expTitle));//false
+console.log("titles are not equal:"+(!(actTitle===expTitle)));//true
 
 
 
@@ -91,3 +267,38 @@ value will use first and then increment
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(10 > 5 > 1);
